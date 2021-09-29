73de94e3b2a31320ad2736177ab7cacf53bf0f1b
MaaaaaZ <90545124+MaaaaaZ@users.noreply.github.com>
2021-09-29 20:19:19 +0800

    Add files via upload

---
+++ 9291/9291/9291.c
@@ -0,0 +1,47 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+////int main()
+//{
+//	char password[20] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s", password);
+//	printf("请确认密码(Y/N):>");
+//	int tmp = 0;
+//	while (tmp = getchar() != '\n')        //清理缓冲区的while循环！！！
+//	{									   //清理缓冲区的while循环！！！
+//		;			   					   //清理缓冲区的while循环！！！
+//	}                                      //清理缓冲区的while循环！！！
+//	int ch = getchar();
+//	if (ch == 'Y')
+//	{
+//		printf("确认成功\n");
+//	}
+//	else
+//	{
+//		printf("确认失败\n");
+//	}
+//	return 0;
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 10; i++)
+	{ 
+		printf("%d", i);
+	}
+	for (;;)
+	{
+		printf("123");
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
